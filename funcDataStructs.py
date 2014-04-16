b29434b88bf4ad348d6feda377c9ba4d3d55a07c
Chen Lin <clin_@berkeley.edu>
2014-04-16 16:53:13 -0400

    Herro.

--- funcDataStructs.py
+++ funcDataStructs.py
@@ -3,17 +3,46 @@ class imVector:
   MAXBITS = 2 #power of two
   MAXLEN = 2 ** MAXBITS # four
   MAXELEM = MAXLEN - 1
-  def __init__(self):
-    self.val = []
-    self.length = 0
-    self.depth = 1
+
+  """
+  ( (1,2,3,4),  )
+  """
+
+  def __init__(self, val=(), length=0, depth=1):
+    self.val = val
+    self.length = length
+    self.depth = depth
+
+  def __str__(self):
+    return str(self.val)
+
+  def __repr__(self):
+    return str(self)
+
   def append(self, element):
-    for k in xrange(self.depth) 
-      newList = []
+    if self.length == imVector.MAXLEN ** self.depth:
+      return imVector((self.val, eval("("*self.depth + "element" + ",)"*self.depth)),
+                      self.length + 1,
+                      self.depth + 1)
 
-      
-    
-   
+    return imVector(imVector._append(element, self.val, 1, self.depth),
+                    length=self.length + 1,
+                    depth=self.depth)
+    # for k in xrange(self.depth)
+    #   newList = []
 
-  
-  
+  @staticmethod
+  def _append(element, node, depth, bottomdepth):
+    if depth == bottomdepth:
+      return node + (element,)
+
+    # if node is empty?
+    end = (imVector._append(element, node[-1], depth+1, bottomdepth),)
+    return node[:-1] + end
+
+def test():
+  v = imVector()
+  for i in range(5):
+    v = v.append(i)
+
+  return v
