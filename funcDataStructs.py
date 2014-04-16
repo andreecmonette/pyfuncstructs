3a7d5fed6f2cc737a030023f6055cc669df84b1e
Andree Monette <andreecmonette@gmail.com>
2014-04-16 16:05:52 -0400

    Initial commit

---
+++ funcDataStructs.py
@@ -0,0 +1,19 @@
+import copy
+class imVector:
+  MAXBITS = 2 #power of two
+  MAXLEN = 2 ** MAXBITS # four
+  MAXELEM = MAXLEN - 1
+  def __init__(self):
+    self.val = []
+    self.length = 0
+    self.depth = 1
+  def append(self, element):
+    for k in xrange(self.depth) 
+      newList = []
+
+      
+    
+   
+
+  
+  
